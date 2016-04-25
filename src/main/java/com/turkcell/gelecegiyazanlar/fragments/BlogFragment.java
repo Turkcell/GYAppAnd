81c46e9f3753b9524984dd306482784e17096b2c
caner <cgural@gmail.com>
2016-04-26 00:26:00 +0300

    BlogEtkinlikListeFragment.java'nın okunabilirliği artırıldı. BlogFragment.java'da deprecated olmuş metotlar güncellendi. API'den çağırırken kullanılan blog kategori numaraları ilgili model nesnesine alındı.

--- src/main/java/com/turkcell/gelecegiyazanlar/fragments/BlogFragment.java
+++ src/main/java/com/turkcell/gelecegiyazanlar/fragments/BlogFragment.java
@@ -1,6 +1,7 @@
 package com.turkcell.gelecegiyazanlar.fragments;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -16,7 +17,7 @@ public class BlogFragment extends android.support.v4.app.Fragment {
     public SlidingTabLayout tabs;
     Toolbar toolbar;
     CharSequence titles[];
-    int numbOfTabs = 5;
+    int numbOfTabs = 8;
 
     ViewPager pager;
     ViewPagerAdapterBlogEtkinlik adapter;
@@ -27,12 +28,17 @@ public class BlogFragment extends android.support.v4.app.Fragment {
 
         toolbar = (Toolbar) getActivity().findViewById(R.id.tool_bar);
         View rootView = inflater.inflate(R.layout.fragment_blog, container, false);
+        //ViewPager'a ait tab'larda görünecek yazýlar
         titles = new CharSequence[]{
+                getString(R.string.blog_kategori_tumu),
                 getString(R.string.blog_kategori_gelecegi_yazanlar),
                 getString(R.string.blog_kategori_mobil),
                 getString(R.string.blog_kategori_android),
                 getString(R.string.blog_kategori_ios),
-                getString(R.string.blog_kategori_wp)
+                getString(R.string.blog_kategori_wp),
+                getString(R.string.blog_kategori_oyun),
+                getString(R.string.blog_kategori_tasarim)
+
         };
 
         // Baþlýklarý,tab sayýsýný adapterda tanýmlar
@@ -45,7 +51,9 @@ public class BlogFragment extends android.support.v4.app.Fragment {
 
         tabs = (SlidingTabLayout) rootView.findViewById(R.id.tabsBlog);
         tabs.setDistributeEvenly(true); // tablarýn düzenlenebilir olmasýný saðlar
-        tabs.setSelectedIndicatorColors(getResources().getColor(R.color.beyaz_renk));
+        //tabs.setSelectedIndicatorColors(getResources().getColor(R.color.beyaz_renk));
+        //bkz: http://stackoverflow.com/questions/31590714/getcolorint-id-deprecated-on-android-6-0-marshmallow-api-23
+        tabs.setSelectedIndicatorColors(ContextCompat.getColor(getActivity(),R.color.beyaz_renk));
 
         tabs.setViewPager(pager);
 
---
+++ src/main/java/com/turkcell/gelecegiyazanlar/models/BlogKategori.java
@@ -0,0 +1,17 @@
+package com.turkcell.gelecegiyazanlar.models;
+
+/**
+ * Created by cgural on 25.04.2016.
+ */
+//Geleceði Yazanlar API'deki blog kategorilerinin sayýsal karþýlýklarý
+public class BlogKategori {
+    public static final int TUMU=0;
+    public static final int GY=718;
+    public static final int MOBIL=31;
+    public static final int ANDROID=29;
+    public static final int IOS=28;
+    public static final int WP=738;
+    public static final int FIREFOXOS=2010;
+    public static final int TASARIM=687;
+    public static final int OYUN=3423;
+}
